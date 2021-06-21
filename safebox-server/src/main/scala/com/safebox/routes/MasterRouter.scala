3791c03eff76968964bcf37cd8d17418ddb46238
Mathew Kuriakose <kuriakosemathew007@gmail.com>
2021-06-21 15:08:10 +0100

    Minor code cleanup

--- safebox-server/src/main/scala/com/safebox/routes/MasterRouter.scala
+++ safebox-server/src/main/scala/com/safebox/routes/MasterRouter.scala
@@ -3,5 +3,5 @@ package com.safebox.routes
 import akka.http.scaladsl.server.Directives._
 
 object MasterRouter {
-  val routes = DownloadRouter.download ~ HealthCheckRouter.route ~ UserRouter.userRoute ~ UploadRouter.uploadRoute ~ DownloadRouter.listfiles
+  val routes = DownloadRouter.download ~ HealthCheckRouter.route ~  UploadRouter.uploadRoute ~ DownloadRouter.listfiles
 }
