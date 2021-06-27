4f9b7e5b417f1e9834d440dcae3b353d116d7efb
Mathew Kuriakose <kuriakosemathew007@gmail.com>
2021-06-27 17:27:57 +0100

    Parameterized file download

--- safebox-server/src/main/scala/com/safebox/routes/DownloadRouter.scala
+++ safebox-server/src/main/scala/com/safebox/routes/DownloadRouter.scala
@@ -2,14 +2,14 @@ package com.safebox.routes
 
 import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Directives.{complete, get, listDirectoryContents, path, respondWithHeaders}
+import akka.http.scaladsl.server.Directives.{complete, get, listDirectoryContents, parameters, path, respondWithHeaders}
 import akka.http.scaladsl.server.{RequestContext, RouteResult}
 import akka.stream.scaladsl.FileIO
 
 import java.io.File
-import scala.concurrent.{Future}
+import scala.concurrent.Future
 
-object DownloadRouter  {
+object DownloadRouter {
 
   def listfiles: RequestContext => Future[RouteResult] = get {
     path("list") {
@@ -18,13 +18,19 @@ object DownloadRouter  {
     }
   }
 
-  def download: RequestContext => Future[RouteResult] = get {
-    path("download") {
-      val file = new File("src/main/resources/download-test.txt")
-      respondWithHeaders(RawHeader("Content-Disposition", s"""attachment; filename="download-test.txt"""")) {
-        complete(HttpEntity(ContentTypes.`application/octet-stream`, FileIO.fromPath(file.toPath)))
+  def download: RequestContext => Future[RouteResult] =
+    parameters("fileName") { fileName =>
+      get {
+        path("download") {
+          val file = new File(s"src/main/resources/$fileName")
+          if (file.exists()) {
+            respondWithHeaders(RawHeader("Content-Disposition", s"""attachment; filename="$fileName"""")) {
+              complete(HttpEntity(ContentTypes.`application/octet-stream`, FileIO.fromPath(file.toPath)))
+            }
+          } else {
+              complete("The Requested file is not available for download")
+          }
+        }
       }
     }
-  }
-
 }
